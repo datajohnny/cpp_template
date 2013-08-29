24b82028564a001feb0b66adbfc1571e060887c1
Johnny Mejias <johnny@Johnnys-MacBook-Pro.local>
2013-08-29 15:20:13 -0400

    Clean start

--- project_test.cpp
+++ project_test.cpp
@@ -1,34 +1,3 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "project.h"
-
-using namespace std;
-
-TEST(Project, ReturnAllProjects) {
-  Project *project = new Project();
-  EXPECT_EQ(project->List(), "All Projects:");
-}
-
-TEST(Project,FindRecord) {
-  Project *project = new Project();
-  EXPECT_EQ(project->Find(1), "Find Project With Id: 1");
-}
-
-TEST(Project, CreateRecord) {
-  string name = "Johnny";
-  string description = "Programmer";
-  Project *project = new Project();
-  EXPECT_TRUE(project->Create(name, description));
-}
-
-TEST(Project, UpdateRecord) {
-  string name = "Johnny";
-  string description = "Programmer";
-  Project *project = new Project();
-  EXPECT_TRUE(project->Create(name, description));
-}
-
-TEST(Project, DeleteRecord) {
-  Project *project = new Project();
-  EXPECT_TRUE(project->Delete(1));
-}
